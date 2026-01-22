d50c1db6385a6a1a617a9fc6c0bebb319ff7484d
VoAnhQuoc11 <quocvogiarai0808@gmail.com>
2026-01-22 21:41:43 +0700

    Delete client directory

--- client/app/src/main/java/com/example/client/model/data/User.kt
+++
@@ -1,8 +0,0 @@
-package com.example.client.model.data
-
-data class User(
-    val id: String = "",
-    val username: String = "",
-    val fullName: String = "",
-    val avatarUrl: String = ""
-)
